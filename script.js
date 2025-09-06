45ffa9d4d24f3b7a7d90386c64ca5af220bfd6c1
arik509 <sabirhossainarik34@gmail.com>
2025-09-06 22:42:29 +0600

    Added Banner

---
+++ script.js
@@ -0,0 +1,7 @@
+const loadLessons = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then(res => res.json())
+    .then(json => console.log(json));
+}
+
+loadLessons();
